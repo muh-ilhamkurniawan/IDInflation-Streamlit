72e8494d9148f6be05b1c0c916fcc423b5ad1161
muh-ilhamkurniawan <93339557+muh-ilhamkurniawan@users.noreply.github.com>
2024-08-15 21:37:32 +0700

    Moving models into folders

--- prediction.py
+++ prediction.py
@@ -4,7 +4,7 @@ import joblib
 # Function to predict inflation based on year and month
 def predict_inflation(year, month):
     # Load the model
-    loaded_rf_model = joblib.load('random_forest_model.pkl')
+    loaded_rf_model = joblib.load('models/random_forest_model.pkl')
 
     input_data = pd.DataFrame({'year': [year], 'month': [month]})
     prediction = loaded_rf_model.predict(input_data)
